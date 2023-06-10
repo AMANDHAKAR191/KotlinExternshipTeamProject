2f0c566a3e99c282a00c4217f057c1cd12e342b4
kiran <kirandhaker123@gmail.com>
2023-06-10 10:34:00 +0530

    added Expanse Data Class testing
    Signed-off-by: kiran <amandhaker191@gmail.com>

---
+++ app/src/main/java/com/externship/kotlinexternshipteamproject/domain/model/Expanse.kt
@@ -0,0 +1,9 @@
+package com.externship.kotlinexternshipteamproject.domain.model
+
+data class Expanse(
+    val date: String,
+    val amount: String,
+    val category: String,
+    val paymentMode: String,
+    val note: String
+)
